483d300f8ea4a37d1cec92e43967bf8e26bfe830
GLKaiky <kaikyfranca8@gmail.com>
2023-06-03 17:05:17 -0300

    Velocimetro incompleto

--- codigo/steero.js
+++ codigo/steero.js
@@ -1,32 +1,37 @@
 
 
+
+
+
 const velocimetro = document.querySelector("#velocimetro")
 const velocidade = document.querySelector("#velocidade")
 
-
 let watchID = null
-const option = {enableHightAccuracy:true}
+const option = {enableHighAccuracy:true}
+
 velocimetro.addEventListener("click",()=>{
-    if(!watchID){
-        watchID = navigator.geolocation.watchPosition(updatePosition, handleError)
-    }else{
-        navigator.geolocation.clearWatch(watchID)
-    watchID = null
-    valocidade.textContent = 0
-    }
+  	console.log(watchID)
+	if(!watchID){
+			watchID = navigator.geolocation.watchPosition(
+ 			 updatePosition, handleError, option)
+	}
+   else{
+			navigator.geolocation.clearWatch(watchID)
+  			watchID = null
+  			velocidade.textContent = 0
+	}
 })
 
-navigator.geolocation.watchPosition(updatePosition, handleError, )
+//navigator.geolocation.watchPosition(
+ // updatePosition, handleError)
 
-
-
-function updatePosition(position){
-    console.log(position.coords.speed * 3.6)
-    if (position.coords.speed === null)
-    return
-    velocidade.textContent = (position.coords.speed * 3.6).toFixed(1)
+function updatePosition(position){ 
+  //console.log(position.coords.speed * 3.6)
+  	if(position.coords.speed === null)
+    		return
+  	velocidade.textContent = (position.coords.speed * 3.6).toFixed(1)
 }
 
 function handleError(error){
-    console.log(error.message)
+		console.log(error.message)
 }
