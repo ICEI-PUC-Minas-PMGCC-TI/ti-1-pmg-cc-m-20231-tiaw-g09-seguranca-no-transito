143ba7b481f3b41064986900702a04d036c1286a
GLKaiky <kaikyfranca8@gmail.com>
2023-06-14 08:39:28 -0300

    Cadastro em progresso

--- codigo/criar.js
+++ codigo/criar.js
@@ -1,18 +1,43 @@
+let nome = document.querySelector('#nome')
+let labelNome = document.querySelector('#labelNome')
 
-function logar(){
-    var login = document.getElementById('login').value;
-    var senha = document.getElementById('senha').value;
+let usuario = document.querySelector('#usuario')
+let labelUsuario = document.querySelector('#labelUsuario')
 
-    if (login == "admin" && senha == "admin") {
-        alert('Sucesso');
-        location.href = "index.html";
+let idade = document.querySelector('#idade')
+let labelIdade = document.querySelector('#labelIdade')
+
+let senha = document.querySelector('#senha')
+let labelSenha = document.querySelector('#labelSenha')
+
+let confirmsenha = document.querySelector('#confirmsenha')
+let labelconfirmSenha= document.querySelector('#labelconfirmSenha')
+
+
+nome.addEventListener('keyup',()=>{
+    if(nome.value.length <= 2){
+     labelNome.setAttribute('style','color: red')
+     labelNome.innerHTML = 'Minimo 3 caracteres'
+     nome.setAttribute('style', 'color: red')
     }else {
-        alert('Usuário ou senha errados! Tente novamente');
-        location.href = "login.html";
+        labelNome.setAttribute('style','color: green')
+        labelNome.innerHTML = 'Nome'
+        nome.setAttribute('style', 'color: green')
     }
-}
+})
 
+usuario.addEventListener('keyup',()=>{
+    if(usuario.value.length <= 1){
+     labelUsuario.setAttribute('style','color: red')
+     labelUsuario.innerHTML = 'Minimo 2 caracteres'
+     usuario.setAttribute('style', 'color: red')
+    }else {
+        labelUsuario.setAttribute('style','color: green')
+        labelUsuario.innerHTML = 'Usuario'
+        usuario.setAttribute('style', 'color: green')
+    }
+})
 
 function cadastrar(){
-    alert('Botao clicado')
+    alert("batata");
 }
